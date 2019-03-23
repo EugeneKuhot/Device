c0b73a8d2a08a2158a7cd7cc47be5ea899b84f3c
Yevhenii <kz.greenlinefamily@gmail.com>
2019-03-23 22:15:47 +0100

    qweqweqweqew

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 var link = document.querySelector(".contacts-btn");
-			var popup = document.querySelector(".modal-write-us");
-			var close = document.querySelector(".modal-close");
-			var linkmap = document.querySelector(".map-link");
-			var popupmap = document.querySelector(".map-popup");
-			var closemap = document.querySelector(".map-close");
+var popup = document.querySelector(".modal-write-us");
+var close = document.querySelector(".modal-close");
+var linkmap = document.querySelector(".map-link");
+var popupmap = document.querySelector(".map-popup");
+var closemap = document.querySelector(".map-close");
 
-			link.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.add("modal-show");
-			});
-			
-			close.addEventListener("click", function (evt) {
-				popup.classList.remove("modal-show");
-			});
 
-			linkmap.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popupmap.classList.add("map-show");
-			});
 
-			closemap.addEventListener("click", function (evt) {
-				popupmap.classList.remove("map-show");
-			});
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+	popup.classList.remove("modal-show");
+});
+
+linkmap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupmap.classList.add("map-show");
+});
+
+closemap.addEventListener("click", function (evt) {
+	popupmap.classList.remove("map-show");
+});
